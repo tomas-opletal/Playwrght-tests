2d2b5698a6991231e0a3d3a3d44f7d429c387ee9
unknown <203537@vutbr.cz>
2024-09-08 14:14:05 +0200

    Delete of comment

--- test_example.py
+++ test_example.py
@@ -3,7 +3,5 @@ from playwright.sync_api import Page, expect
 
 def test_has_title(page: Page):
     page.goto("https://vuejs.org/examples/#form-bindings")
-
-    # Expect a title "to contain" a substring.
     assert page.title() == "Examples | Vue.js"
 
